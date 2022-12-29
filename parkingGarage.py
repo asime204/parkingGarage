599391999126ac721708003d6128bfc05a832ca7
asime204 <azielsime1@gmail.com>
2022-12-28 19:05:57 -0500

    created class skeleton

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,4 +1,20 @@
 class ParkingGarage:
-    pass
+    def __init__(self, size):
+        self.tickets = [{"paid": False} for i in range(size)]
+        self.parkingSpaces = [{} for i in range(size)]
+        self.currentTicket = {}
 
-#todd's test
+    def take_ticket(self):
+        if self.tickets:
+            self.currentTicket = self.tickets.pop()
+            self.parkingSpaces.pop()
+
+    def payForParking(self):
+        pass
+
+    def leaveGarage(self):
+        pass
+
+myGarage = ParkingGarage(3)
+
+print(myGarage.tickets)
